127065bc66fda9bb02f1f7208cdf3cf2e701c275
tommccoy1 <tom.mccoy@jhu.edu>
2020-06-24 13:30:31 +0000

    update saving directory

--- lm_train.sh
+++ lm_train.sh
@@ -8,7 +8,7 @@ python preprocess.py \
 
 python train.py --task language_modeling \
     /mnt/my_output/data-bin/wikitext-103-padded \
-    --save-dir /mnt/my_output/knn-checkpoints-padded/ \
+    --save-dir /mnt/my_output/knn-checkpoints-padded-hypercluster/ \
     --arch transformer_lm_wiki103 \
     --max-update 300000 --max-lr 1.0 --t-mult 2 --lr-period-updates 270000 --lr-scheduler cosine --lr-shrink 0.75 \
     --warmup-updates 16000 --warmup-init-lr 1e-07 --min-lr 1e-09 --optimizer nag --lr 0.0001 --clip-norm 0.1 \
