57a6a9c2603e9693a0f87b0891aea9dacc6a9332
tommccoy1 <tom.mccoy@jhu.edu>
2020-06-24 06:18:08 +0000

    remove fp16

--- lm_train.sh
+++ lm_train.sh
@@ -12,6 +12,6 @@ python train.py --task language_modeling \
     --arch transformer_lm_wiki103 \
     --max-update 300000 --max-lr 1.0 --t-mult 2 --lr-period-updates 270000 --lr-scheduler cosine --lr-shrink 0.75 \
     --warmup-updates 16000 --warmup-init-lr 1e-07 --min-lr 1e-09 --optimizer nag --lr 0.0001 --clip-norm 0.1 \
-    --criterion adaptive_loss --max-tokens 3072 --update-freq 3 --tokens-per-sample 3072 --seed 1 --fp16 \
+    --criterion adaptive_loss --max-tokens 3072 --update-freq 3 --tokens-per-sample 3072 --seed 1  \
     --sample-break-mode none --skip-invalid-size-inputs-valid-test --ddp-backend=no_c10d --dropout 0.0 \
     --activation-dropout 0.0 --adaptive-softmax-dropout 0.0 --attention-dropout 0.0 
